3d1729055bf5998c4b9856368cea496ddb6ce3c5
danny-demarco <danny.demarco@protonmail.com>
2023-03-20 21:22:58 +0000

    Add models

--- tile_api_app/models.py
+++ tile_api_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Tile(models.Model):
+    
+    class Status(models.TextChoices):
+        LIVE = 'live', 'Live'
+        PENDING = 'pending', 'Pending'
+        ARCHIVED = 'archived', 'Archived'
+
+    launch_date = models.DateField()
+    status = models.CharField(max_length=10, choices=Status.choices, default=Status.PENDING)
+
+
+class Task(models.Model):
+
+    class Type(models.TextChoices):
+        SURVEY = 'survey', 'Survey'
+        DISCUSSION = 'discussion', 'Discussion'
+        DIARY = 'diary', 'Diary'
+
+    title = models.CharField(max_length=255)
+    order = models.IntegerField() # The spec is unclear on the use case for this field so it may need refactoring
+    description = models.TextField(max_length=1000)
+    type = models.CharField(max_length=10, choices=Type.choices, default=Type.SURVEY)
+    tile = models.ForeignKey(Tile, on_delete=models.CASCADE)
+
+
+
